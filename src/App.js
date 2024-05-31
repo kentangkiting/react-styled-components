701fb180a2540f41ab603e0ba8d2a069951d11c7
kentangkiting <agustinus_bintang@yahoo.com>
2024-05-31 18:08:36 +0700

    clear page add header and butons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
+
+const Button = styled.button`
+  &:hover {
+    background-color: white;
+  }
+  background-color: ${(props) => (props.isLogin ? "palevioletred" : "grey")};
+  padding: 16px;
+  border-radius: 10px;
+  margin: 2%;
+`;
+
+const HeaderPertama = styled.h1`
+  font-size: 3em;
+`;
+
+const Container = styled.div`
+  background-color: rgba(128, 128, 128, 0.5);
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <HeaderPertama>Halaman Utama</HeaderPertama>
+        <Button isLogin>Button Login</Button>
+        <Button>Logout</Button>
+      </Container>
     </div>
   );
 }
